a38d809eb0bdd9353ac25d83074ed40a595490f6
Matt Farmer <mrfarmer777@gmail.com>
2018-06-27 19:53:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:num1/:num2" do   #! Woops, forgot a forward slash on the route
+  get "/multiply/:num1/:num2" do   #! Woops, forgot a forward slash on the route   
     res=params[:num1].to_i*params[:num1].to_i
     "#{res}"
   end
